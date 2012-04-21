42c030280ca1b816cc28e00a0e5e3907f5699f3a
Andy Joslin <andytjoslin@gmail.com>
2012-04-20 21:32:14 -0400

    added demo .html

--- dragit.js
+++ dragit.js
@@ -5,9 +5,7 @@
  *  License: GPL
  */
 
-// the semi-colon before function invocation is a safety net against concatenated 
-// scripts and/or other plugins which may not be closed properly.
-;(function ( $, window, document, undefined ) {
+(function ( $, window, document, undefined ) {
 
 	// undefined is used here as the undefined global variable in ECMAScript 3 is
 	// mutable (ie. it can be changed by someone else). undefined isn't really being
